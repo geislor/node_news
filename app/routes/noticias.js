6362e7b1a9da6e667fe2e5176a13f78d553399a0
Geislor Crestani <geislor@gmail.com>
2018-07-29 20:28:46 -0300

    add db connection

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,19 @@
 module.exports = function(app){
+
     app.get('/noticias', function(req, res){
-        res.render("noticias/noticias");
+
+        var mysql = require('mysql');
+
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'crestani',
+            password: 'my1313',
+            database: 'portal_noticias',
+            insecureAuth : true
+        });
+
+        connection.query('select * from noticias', function(error, result){
+            res.render("noticias/noticias", {noticias : result});
+        });
     });
 };
