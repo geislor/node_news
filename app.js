a4653adf93771e66192f12dea8a816917a4058bf
Geislor Crestani <geislor@gmail.com>
2018-07-30 21:31:43 -0300

    add consign

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 var app = require('./config/server'); 
 
-var routeHome = require('./app/routes/home')(app); 
-var routeNoticies = require('./app/routes/noticias')(app); 
-var routeFormAddNoticies = require('./app/routes/formulario_inclusao_noticia')(app); 
-
 app.listen(3000, function(){
     console.log('Server Running');
 });
