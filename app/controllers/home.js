b01bb1981e839eca12247e20dd6143b0b114ebf0
Geislor Crestani <geislor@gmail.com>
2018-08-07 22:08:45 -0300

    refactor index

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,3 +1,8 @@
 module.exports.index = function(app, req, res){
-    res.render("home/index");
+    var connection = app.config.db();
+    var noticiasModel = new app.app.models.NoticiasDAO(connection);
+
+    noticiasModel.getLastFive(function(err, result){
+        res.render("home/index", {noticias : result});
+    });
 }
--- app/models/NoticiasDAO.js
+++ app/models/NoticiasDAO.js
@@ -10,6 +10,11 @@ NoticiasDAO.prototype.getNoticia = function(callback) {
     this._connection.query('select * from noticias where id_noticia = 1', callback);
 }
 
+
+NoticiasDAO.prototype.getLastFive = function(callback){
+    this._connection.query('select * from noticias order by data_criacao desc limit 5', callback);
+}
+
 NoticiasDAO.prototype.saveNoticia = function(noticia, callback) {
     this._connection.query('insert into noticias set ?', noticia, callback);
 }
